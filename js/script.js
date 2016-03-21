7e70a0c61b00cebd39206fb6fa0684ed4b51989b
Rogatnev Nikita <mail@rogatnev.ru>
2016-03-21 17:56:56 +0300

    Added json

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 /* map */
 L.mapbox.accessToken = 'pk.eyJ1IjoieWJyYm5maGp1Zm55dGQiLCJhIjoiY2lsdzJnZHdtMDA4eHZmbTY5YTlvaTZtdSJ9.V8utWdptmcKtbUYAd3Y0yw';
-var map = L.mapbox.map('map', 'ybrbnfhjufnytd.pfed98ij').setView([40, 50], 4);
-/* L.mapbox.styleLayer('mapbox://styles/mapbox/emerald-v8').addTo(map); */
+var map = L.mapbox.map('map').setView([40, 50], 4);
+/*var map = L.mapbox.map('map', 'ybrbnfhjufnytd.pfed98ij').setView([40, 50], 4);*/
+L.mapbox.styleLayer('mapbox://styles/ybrbnfhjufnytd/cim23zy5d00rlbkkk05etz3qm').addTo(map);
+var featureLayer = L.mapbox.featureLayer().loadURL('./markers.json').addTo(map);
 
 /* marker-list */
 var markerList = document.getElementById('marker-list');
